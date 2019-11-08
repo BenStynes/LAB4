7ed586c543c85d7cd439451d39b1540248e352c2
MuddyGames <make@muddygames.com>
2019-11-08 11:01:12 +0000

    Added Circle Collision

--- CollisionDetection/Source.cpp
+++ CollisionDetection/Source.cpp
@@ -18,19 +18,22 @@ public:
 	int r;
 
 	void print() {
-		std::cout << "x:" << p.x << " y:" << p.y << " r:" << p.r << std::endl;
+		std::cout << "x:" << p.x << " y:" << p.y << " r:" << r << std::endl;
 	}
 };
 
 int distance(Point a, Point b) {
 	int result = 0;
+
+	// Find the distance (Length) of X and Y  
+
 	int x_Distance = b.x - a.x;
 	int y_Distance = b.y - a.y;
 
 	// Now that we have the distance (Length)
-	// between point a and b
-	// Use Pythagoras theorem to get the distance between
-	// the Point a and Point b 
+	// of x and y, use Pythagoras theorem 
+	// to get the distance (length of hypotenuse) 
+	// between Point a and Point b 
 	result = sqrt(pow(x_Distance, 2) + pow(y_Distance, 2));
 
 	return result;
@@ -41,49 +44,66 @@ int main() {
 
 	srand(time(NULL));
 
-	int player_x_coordinate = rand() % 10 + 1;
-	int player_y_coordinate = rand() % 10 + 1;
+	// Point Collision
+	Point player_point;
+	player_point.x = rand() % 10 + 1;
+	player_point.y = rand() % 10 + 1;
 
-	Point one;
-	one.x = player_x_coordinate;
-	one.y = player_y_coordinate;
+	Point npc_point;
+	npc_point.x = rand() % 10 + 1;
+	npc_point.y = rand() % 10 + 1;
 
-	int npc_x_coordinate = rand() % 10 + 1;
-	int npc_y_coordinate = rand() % 10 + 1;
+	// Circle Collision
+	Circle player_circle;
+	player_circle.p.x = rand() % 10 + 1;
+	player_circle.p.y = rand() % 10 + 1;
+	player_circle.r = rand() % 2 + 1;
+	
+	Circle npc_circle;
+	npc_circle.p.x = rand() % 10 + 1;
+	npc_circle.p.y = rand() % 10 + 1;
+	npc_circle.r = rand() % 2 + 1;
 
-	Point two;
-	two.x = npc_x_coordinate;
-	two.y = npc_y_coordinate;
 
 	while (true) {
-		if (distance(one, two) == 0)
+
+		// Point Collision check
+		if (distance(player_point, npc_point) == 0)
 		{
-			std::cout << "Collision Point" << std::endl;
-			one.print();
-			two.print();
+			std::cout << "Point Collision" << std::endl;
+			player_point.print();
+			npc_point.print();
 			std::cin.get();
 		}
 		else
 		{
-			std::cout << "No Collision Point" << std::endl;
-			one.print();
-			two.print();
+			std::cout << "No Collision" << std::endl;
+			player_point.print();
+			npc_point.print();
 		}
 
-		player_x_coordinate = rand() % 10 + 1;
-		player_y_coordinate = rand() % 10 + 1;
+		player_point.x = rand() % 10 + 1;
+		player_point.y = rand() % 10 + 1;
 
-		one.x = player_x_coordinate;
-		one.y = player_y_coordinate;
+		// Circle Collision check
+		if (distance(player_circle.p, npc_circle.p) < (player_circle.r + npc_circle.r))
+		{
+			std::cout << "Circle Collision" << std::endl;
+			player_circle.print();
+			npc_circle.print();
+			std::cin.get();
+		}
+		else
+		{
+			std::cout << "No Collision" << std::endl;
+			player_circle.print();
+			npc_circle.print();
+		}
 
-		npc_x_coordinate = rand() % 10 + 1;
-		npc_y_coordinate = rand() % 10 + 1;
+		player_circle.p.x = rand() % 10 + 1;
+		player_circle.p.y = rand() % 10 + 1;
 
-		two.x = npc_x_coordinate;
-		two.y = npc_y_coordinate;
 	}
-
-	std::cin.get();
 
 	return 0;
 }
