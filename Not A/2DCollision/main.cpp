742985ed7f0bf002170066e58ada186cbea353e8
(Student) - Ben Stynes <c00239718@ITCARLOW.IE>
2019-11-18 10:56:17 +0000

    frucking yeet

--- Not A/2DCollision/main.cpp
+++ Not A/2DCollision/main.cpp
@@ -68,7 +68,54 @@ int main()
 	npc_box.setOutlineColor(sf::Color::Green);
 	npc_box.setSize(sf::Vector2f{ 84,84 });
 	npc_box.setFillColor(sf::Color{ 0,0,0,0 });
+	//setup capsule
 
+	sf::CircleShape top;
+	sf::CircleShape bottom;
+	sf::RectangleShape body;
+	top.setFillColor(sf::Color::White);
+	bottom.setFillColor(sf::Color::White);
+	body.setFillColor(sf::Color::White);
+	top.setRadius(30);
+	bottom.setRadius(30);
+	body.setSize(sf::Vector2f{ 60,30 });
+	top.setPosition(200, 180);
+	body.setPosition(200, 210);
+	bottom.setPosition(200, 200);
+	
+	//Poly Setup, want a cracker?
+
+	sf::VertexArray pointless{ sf::Triangles ,3};
+	pointless.append({ sf::Vector2f{ 400,300 },sf::Color::White });
+	pointless.append({ sf::Vector2f{ 300,200 },sf::Color::White });
+	pointless.append({ sf::Vector2f{ 400,400 },sf::Color::White });
+	c2Poly poly_npc;
+	
+	poly_npc.count = 3;
+	poly_npc.verts[0] = c2v{ 400,300 };
+	poly_npc.verts[1] = c2v{ 300,200 };
+	poly_npc.verts[2] = c2v{ 400,400 };
+	//capsule setip 2
+	c2Capsule capsule_npc;
+	capsule_npc.a.x = top.getPosition().x + top.getRadius();
+	capsule_npc.a.y = top.getPosition().y + top.getRadius();
+
+	capsule_npc.b.x = bottom.getPosition().x + bottom.getRadius();
+	capsule_npc.b.y = bottom.getPosition().y + bottom.getRadius();
+
+	capsule_npc.r = 30;
+
+	//ray setup;
+	sf::VertexArray pointless2{ sf::Lines ,2 };
+	pointless2.append({ sf::Vector2f{ 100,300 },sf::Color::White });
+	pointless2.append({ sf::Vector2f{ 420,300 },sf::Color::White });
+
+	c2Ray raymond;
+	raymond.p = c2v{ 100,300 };
+	raymond.d = { c2Norm(c2v{100,300}) };
+	raymond.t = {  };
+
+	c2Raycast re;
 	//Setup Player AABB
 	c2AABB aabb_player;
 	aabb_player.min = c2V(player.getAnimatedSprite().getPosition().x, player.getAnimatedSprite().getPosition().y);
@@ -197,7 +244,8 @@ int main()
 		result = c2AABBtoAABB(aabb_player, aabb_npc);
 		cout << ((result != 0) ? ("Collision") : "") << endl;
 		if (result){
-			player.getAnimatedSprite().setColor(sf::Color(255,255,255));
+			player.getAnimatedSprite().setColor(sf::Color(255, 255, 255));
+
 			npc_box.setOutlineColor(sf::Color::Green);
 			player_box.setOutlineColor(sf::Color::Green);
 		}
@@ -206,7 +254,63 @@ int main()
 			player_box.setOutlineColor(sf::Color::White);
 			npc_box.setOutlineColor(sf::Color::White);
 		}
+		
+		//capsule collison
+		if (c2AABBtoCapsule(aabb_player, capsule_npc))
+		{
+			player.getAnimatedSprite().setColor(sf::Color(255, 255, 255));
 
+			player_box.setOutlineColor(sf::Color::Green);
+			top.setFillColor(sf::Color::Red);
+			body.setFillColor(sf::Color::Red);
+			bottom.setFillColor(sf::Color::Red);
+		}
+
+		else
+		{
+		
+			top.setFillColor(sf::Color::White);
+			body.setFillColor(sf::Color::White);
+			bottom.setFillColor(sf::Color::White);
+		}
+		//poly collsioin
+		if (c2AABBtoPoly(aabb_player, &poly_npc,NULL))
+		{
+			player.getAnimatedSprite().setColor(sf::Color(255, 255, 255));
+
+			player_box.setOutlineColor(sf::Color::Green);
+			pointless.clear();
+			pointless.append({ sf::Vector2f{ 400,300 },sf::Color::Red });
+			pointless.append({ sf::Vector2f{ 300,200 },sf::Color::Red });
+			pointless.append({ sf::Vector2f{ 400,400 },sf::Color::Red });
+		
+		}
+
+		else
+		{
+
+			pointless.append({ sf::Vector2f{ 400,300 },sf::Color::White });
+			pointless.append({ sf::Vector2f{ 300,200 },sf::Color::White });
+			pointless.append({ sf::Vector2f{ 400,400 },sf::Color::White });
+		}
+
+		if (c2RaytoAABB(raymond, aabb_player,&re))
+		{
+			player.getAnimatedSprite().setColor(sf::Color(255, 255, 255));
+
+			player_box.setOutlineColor(sf::Color::Green);
+			pointless2.clear();
+			pointless2.append({ sf::Vector2f{ 100,300 },sf::Color::Red });
+			pointless2.append({ sf::Vector2f{ 420,300 },sf::Color::Red });
+
+		}
+
+		else
+		{
+
+			pointless2.append({ sf::Vector2f{ 100,300 },sf::Color::White });
+			pointless2.append({ sf::Vector2f{ 420,300 },sf::Color::White });
+		}
 		// Clear screen
 		window.clear();
 
@@ -223,6 +327,13 @@ int main()
 			// Draw the NPC's Current Animated Sprite
 			window.draw(npc.getAnimatedSprite());
 		}
+		
+		window.draw(body);
+		window.draw(top);
+		window.draw(bottom);
+	
+		window.draw(pointless);
+		window.draw(pointless2);
 		// Update the window
 		window.display();
 	}
